4fe5172fd23f475c0b127747934e08c5e40ef943
Justin Turner <jmturner638@gmail.com>
2021-07-31 09:20:16 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,19 @@
 def main():
     #write your code below this line
+    numbers = []
+
+    while True:
+        number = int(input())
+
+        if number == -1:
+            break
+
+        numbers.append(number)
+
+    query = int(input("Search for?"))
+    for i in range(len(numbers)):
+        if numbers[i] == query:
+            print("{} is at index {}".format(query, i))
 
 if __name__ == '__main__':
     main()
